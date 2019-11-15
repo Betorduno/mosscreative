960a7a73b3f8788de275ff676931126d881354ac
Alberto <alberto.rodriguez.davila@gmail.com>
2019-11-15 17:36:19 -0500

    avance galeria portafolioo

--- components/portfolioOne/portfolioOneGallery.vue
+++ components/portfolioOne/portfolioOneGallery.vue
@@ -1,21 +1,26 @@
 <template>
-
+<div>
         <section id="portfolio-single-gallery">
 				
 					<h2 class="ps-gallery-heading">Gallery</h2>
 					
-					<div class="owl-carousel popup-gallery nav-outside-top dots-right dots-rounded" data-items="3" data-nav="true" data-dots="false" data-tablet-landscape="3" data-tablet-portrait="3" data-mobile-landscape="2" data-mobile-portrait="1">
-
+					<div  class="owl-carousel popup-gallery nav-outside-top dots-right dots-rounded" data-items="3" data-nav="true" data-dots="false" data-tablet-landscape="3" data-tablet-portrait="3" data-mobile-landscape="2" data-mobile-portrait="1">
+					<div>	
 						<!-- Begin carousel item -->
-						<a href="" class="cc-item ps-gallery-item padding-height-75 popup-trigger bg-image" style="background-image: url('assets/img/porfolio/gallery/portfolio-1/p1-gallery-1.jpg'); background-position: 50% 50%" data-title="by: yogurt86">
+						<router-link v-for="project in projects[1].mainImage" :to ="project.fields.file.url"  :key="project.fields.title" class="cc-item ps-gallery-item padding-height-75 popup-trigger bg-image" :style="'background-image: url('+ project.fields.file.url +'); background-position: 50% 50%;'" data-title="by: yogurt86">
+							
 							<div class="ps-gallery-read-more"><i class="fas fa-search"></i></div>
-						</a>
+						</router-link>
+					</div>
 						<!-- End carousel item -->
+
+
 					</div>
 					<!-- End content carousel -->
 
 				</section>
-  
+			<!-- <div v-for="project in projects[1].mainImage"><pre>-----{{project.fields}}------</pre></div> -->
+ </div>
 </template>
 <script>
 export default {
