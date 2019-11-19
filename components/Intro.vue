7c2b1bec171fb4932a33a8ca4a326af1a60c39e1
Alberto <alberto.rodriguez.davila@gmail.com>
2019-11-19 15:12:01 -0500

    Actulizacion menu principal

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,44 +1,57 @@
 <template>
-   <div>
+  <div>
     <section id="intro" class="slideshow bg-dark">
-    <div class="owl-carousel cursor-grab fade-out-scroll-5 " data-items="1" data-dots="false" data-nav="true">
-           <div v-if="loading">loading...</div>
-						<!-- Begin carousel item 
-						========================= -->
-						<div v-else v-for="slider in sliders" :key="slider.id" class="cc-item parallax bg-image"  :style="'background-image: url(assets/img/intro/'+ slider.image +'); background-position: 50% 50%;'" data-percent-height="1">
-							<!-- Begin caption -->
-							<div class="cc-caption bottom-left caption-animate">
-								<h1 class="page-header-title">title</h1>
-								<div class="intro-sub-title-wrap max-width-400">
-									<!-- max width class is optional -->
-									<h2 class="page-header-sub-title">
-									subtitle
-									</h2>
-								</div>
-						  </div>
-						</div>
+      <div
+        class="owl-carousel cursor-grab fade-out-scroll-5"
+        data-items="1"
+        data-dots="false"
+        data-nav="true"
+      >
+        <div v-if="loading">loading...</div>
+        <!-- Begin carousel item 
+        =========================-->
+        <div
+          v-else
+          v-for="dato in datos"
+          :key="dato.fields.title"
+          class="cc-item parallax bg-image"
+          :style="'background-image: url('+ dato.fields.file.url +'); background-position: 50% 50%;'"
+          data-percent-height="1"
+        >
+          <!-- Begin caption -->
+          <div class="cc-caption bottom-left caption-animate">
+            <h1 class="page-header-title">{{dato.fields.title}}</h1>
+            <div class="intro-sub-title-wrap max-width-400">
+              <!-- max width class is optional -->
+              <h2 class="page-header-sub-title">{{dato.fields.description}}</h2>
             </div>
-	    </section>	
-    </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 <script>
-import data from '../static/assets/js/data.json';
-export default {
-    data() {
-      return {
-        loading: true,
-        sliders: []
-      }
-    },
 
-    mounted() {
-  
-       setTimeout(() => {
-        this.sliders = data;
-        this.loading = false;
-        this.$nextTick(() => {
-          window.dispatchEvent(new Event('resize'));
-          $(this.$el).find('.owl-carousel').owlCarousel({
+import contentful from "@/plugins/contentful.js";
+export default {
+  data() {
+    return {
+      loading: true,
+      datos: []
+    };
+  },
+
+  mounted() {
+    this.getData();
+    setTimeout(() => {
+      
+      this.loading = false;
+      this.$nextTick(() => {
+        window.dispatchEvent(new Event("resize"));
+        $(this.$el)
+          .find(".owl-carousel")
+          .owlCarousel({
             items: 1,
             loop: true,
             margin: 0,
@@ -65,25 +78,38 @@ export default {
               }
             }
           });
+      });
+    }, 500);
+
+    let owl = $(".owl-carousel");
+    owl.on("changed.owl.carousel", this.changeSlider);
+  },
+  methods: {
+    getData() {
+      contentful
+        .getEntries({ content_type: "sliders" })
+        .then(v => {
+        
+        v.items.forEach(element => {
+            this.datos= element.fields.imageMain;
+            
+          });
+        })
+        .catch(error => {
+          alert(error);
         });
-      }, 500);
-
-      let owl = $('.owl-carousel');
-        owl.on('changed.owl.carousel', this.changeSlider)
+        
     },
-    methods: {
-        changeSlider(event) {
-         
-            let change = (event.item.index);
-            let result = false
-            if(change%2==0){
-             // console.log(change);
-              result = true
-            }
+    changeSlider(event) {
+      let change = event.item.index;
+      let result = false;
+      if (change % 2 == 0) {
+        // console.log(change);
+        result = true;
+      }
 
-            this.$emit('changeSlider', result);
-          
-        }      
+      this.$emit("changeSlider", result);
     }
-}
+  }
+};
 </script>
